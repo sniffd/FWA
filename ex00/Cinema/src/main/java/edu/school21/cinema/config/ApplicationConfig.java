ef3845294965e42a30d1b2b84e3d8755b2fdfb12
morrkof <morrkof@gmail.com>
2022-01-25 00:52:19 +0300

    Добавлен dataSource, пропертя для них, конфигурация с бинами, модель User и UserRepository с интерфейсом и реализацией

--- ex00/Cinema/src/main/java/edu/school21/cinema/config/ApplicationConfig.java
+++ ex00/Cinema/src/main/java/edu/school21/cinema/config/ApplicationConfig.java
@@ -1,10 +1,52 @@
 package edu.school21.cinema.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import edu.school21.cinema.repositories.UserRepositoryImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Configuration
+@ComponentScan("edu.school21.cinema")
 @PropertySource("classpath:application.properties")
 public class ApplicationConfig {
-    //TODO datasource
-    //TODO encrypt passwords
-    //TODO  data for connection to db in application.properties
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.user}")
+    private String user;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.driver.name}")
+    private String driverName;
+
+    @Bean
+    public HikariDataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(url);
+        ds.setUsername(user);
+        ds.setPassword(password);
+        ds.setDriverClassName(driverName);
+        return ds;
+    }
+
+    @Bean
+    public PasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserRepositoryImpl usersRepository() throws SQLException, IOException {
+        return new UserRepositoryImpl();
+    }
 }
+
